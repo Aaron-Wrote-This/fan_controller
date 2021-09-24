58215035063b8a6ae4f12ef670eca8519841d151
Aaron-Wrote-This <13478643+Aaron-Wrote-This@users.noreply.github.com>
2021-09-24 13:30:03 -0400

    Restructure and remove unneeded testing in boot.py
    * Add ide folder to gitignore

--- boot.py
+++ boot.py
@@ -1,10 +1,25 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 # import esp
 # esp.osdebug(None)
-import gc
-import NetworkConnect
+
+
 # import webrepl
 # webrepl.start()
-gc.collect()
 
+# import gc
+# gc.collect()
+
+import NetworkConnect
 NetworkConnect.connect_to_network()
+
+# import time
+# import machine
+# blueled = machine.Pin(2, machine.Pin.OUT)
+
+# for i in range(3):
+#     blueled.off()
+#     time.sleep(0.5)
+#     blueled.on()
+#     time.sleep(0.5)
+
+print("DONE!")
