98866390a4b9508d3e935eb6b402c857036f5683
Aaron-Wrote-This <13478643+Aaron-Wrote-This@users.noreply.github.com>
2021-09-24 13:32:44 -0400

    Add method for processing mqtt commands

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from NetworkConnect import get_server_ip
 # mosquitto_pub -t ACCommands -m "on"
 SERVER = get_server_ip()                            # Server IP address
 CLIENT_ID = ubinascii.hexlify(unique_id())          # ID of client to be used by MQTT Server
-TOPIC_KEEPALIVE = b"keepAlive"                   # bedroomEnvironment, "livingroomEnvironment", "basementEnvironment"
-TOPIC_AC = b'ACCommands'                            # Topic for on/off command
-AC_COMMAND = b''
-KEEP_ALIVE = b'0'
+# TOPIC_KEEPALIVE = b"keepAlive"                   # bedroomEnvironment, "livingroomEnvironment", "basementEnvironment"
+# KEEP_ALIVE = b'0'
 
+TOPIC_FAN_PLUG = b'FanCommands'                            # Topic for on/off command
+FAN_COMMAND = b''
 
 
 def process_pushbutton(button_pin, relay_pin, prev_button_state, prev_time, debounce_time):
@@ -27,55 +27,42 @@ def process_pushbutton(button_pin, relay_pin, prev_button_state, prev_time, debo
     prev_button_state = button_state    # record the button state to see if we've processed this one yet
     return prev_button_state, prev_time
 
-    try:
-        for pin in list_of_pins:  # setup all pins as outputs and set them to off position
-            initialized_list_of_pins.append(Pin(pin, Pin.OUT))           # annoyingly Pin (cap) is a function
-            print("Set pin " + str(pin) + " to out")
 
-        for pin in range(0, 4):
-            initialized_list_of_pins[pin](0)
-
-        list_of_state_positions = range(0, 4)  # indexing pin states so it can be reversed easily
-        for x in range(rotation_amt):  # inside /4 is because the "pin in range" does 4 steps at once,
-            for state in list_of_state_positions[::direction]:  # the outer is to make one quarter of a revolution in total
-                for pin in range(0, 4):
-                    initialized_list_of_pins[pin](list_of_states[state][pin])  # setting pin {list of pins} to state {list of states}
-                sleep(speed)
-        # in thhis version motor doesn''t return I think
-        # for x in range(int((one_revolution / 4) / 4)):  # reversing motor back to original location
-        #     for state in list_of_state_positions[::int((-1) * direction)]:
-        #         for pin in range(0, 4):
-        #             initialized_list_of_pins[pin](list_of_states[state][pin])
-        #         sleep(speed)
-    finally:
-        for pin in range(0, 4):
-            initialized_list_of_pins[pin](0)
-            print("Set pin " + str(pin) + " to off")
-    return '1'  # ideally would somehow make blocking, idk how to do that, might auto happen?
+def toggle_relay_from_message(relay_pin: Pin):
+    global FAN_COMMAND
+    if FAN_COMMAND != b'':
+        print('toggleing relay because of mqtt command')
+        if FAN_COMMAND == b'1' and relay_pin.value() != 1:
+            relay_pin.on()
+        elif FAN_COMMAND == b'0' and relay_pin.value() != 0:
+            relay_pin.off()
+        else:
+            print("no toggle needed, was already set correctly")
+    FAN_COMMAND = b''
 
 
 def receive_message(topic, msg):
     # print("{} : {}".format(topic, msg))
-    if topic == TOPIC_AC:
+    if topic == TOPIC_FAN_PLUG:
         # print("Correct topic")
-        global AC_COMMAND
-        if AC_COMMAND == b'':
+        global FAN_COMMAND
+        if FAN_COMMAND == b'':
             # print("AC topic is null(good)")
             if msg == b'on':
                 # print("msg was on good!\n\n\n")
-                AC_COMMAND = b'0'
+                FAN_COMMAND = b'1'
             elif msg == b'off':
-                AC_COMMAND = b'1'
-    elif topic == TOPIC_KEEPALIVE:
-        global KEEP_ALIVE
-        KEEP_ALIVE = b'1'
+                FAN_COMMAND = b'0'
+    # elif topic == TOPIC_KEEPALIVE:
+    #     global KEEP_ALIVE
+    #     KEEP_ALIVE = b'1'
 
 
 def connect_c(c):                                        # connect call, works mostly
     c.connect()
     c.set_callback(receive_message)
-    c.subscribe(TOPIC_AC)
-    c.subscribe(TOPIC_KEEPALIVE)
+    c.subscribe(TOPIC_FAN_PLUG)
+    # c.subscribe(TOPIC_KEEPALIVE)
 
 
 def ping():
